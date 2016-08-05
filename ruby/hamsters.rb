f654ac8a8a25536bc8c016ab78212b097cf3d725
Paul Liu <paul.pliu87@gmail.com>
2016-08-05 08:34:43 -0600

    Add code to ask client to enter the hamster's information

---
+++ ruby/hamsters.rb
@@ -0,0 +1,24 @@
+puts "what is hamser's name?"
+
+hamster_name = gets.chomp
+
+puts "What is the volume level of the hamster?"
+
+hamster_volume = gets.chomp
+
+puts "What is hamster's fur color?"
+
+hamster_color = gets.chomp
+
+puts "Is the hamster a good candidate or adoption?"
+
+adoption = gets.chomp
+
+puts "How old is the hamster?"
+
+hamster_age = gets.chomp
+
+if hamster_age == ""
+	hamster_age = nil
+end
+

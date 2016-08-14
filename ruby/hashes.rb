7f210e8721a91e485328ff64f74b925cb685f21d
Paul Liu <paul.pliu87@gmail.com>
2016-08-14 13:52:22 -0600

    add code to ask client's iformation

---
+++ ruby/hashes.rb
@@ -0,0 +1,16 @@
+applicant = {}
+
+
+puts "Please enter client's name:"
+client[:name] = gets.chomp
+puts "Please enter client's age:"
+client[:age] = get.chomp
+puts "Please enter number of children client has:"
+client[:number_children] = get.chomp
+puts "Please enter if client lives pets:"
+client[:likes_pets] = get.chomp
+
+
+
+
+p applicant

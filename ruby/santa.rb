dd01741b9e943e2df6d34b76536ee0cb215fad67
Paul Liu <paul.pliu87@gmail.com>
2016-08-21 14:38:55 -0600

    add code to initialize santa

---
+++ ruby/santa.rb
@@ -0,0 +1,20 @@
+class Santa
+	def initialize 
+		puts "Initializing Santa instance ..."
+	end
+
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}!}"
+	end
+
+end
+
+
+paul = Santa.new
+
+paul.speak
+paul.eat_milk_and_cookies("snickerdoodle")

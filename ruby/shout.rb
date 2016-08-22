247ca2e17199a0e6dd7af616ca70704f3afa8eeb
Paul Liu <paul.pliu87@gmail.com>
2016-08-21 20:38:16 -0600

    add another method to the module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,13 @@
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yelling_happily(words)
+  	puts"Hello World!!!!"+words
+  end
+
 end
+
+p Shout.yelling_happily("Nice!")
+p Shout.yell_angrily("Paul")

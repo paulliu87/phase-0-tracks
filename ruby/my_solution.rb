8574ee6155a0d12f85afab2843b8d69a2896db2c
Paul Liu <paul.pliu87@gmail.com>
2016-08-22 20:52:59 -0600

    Release4 Comment each method

--- ruby/my_solution.rb
+++ ruby/my_solution.rb
@@ -10,19 +10,21 @@ require_relative 'state_data'
 
 class VirusPredictor
 
+#initialize the object with three instance variables which assigned by the parameters.
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
-
+#define instance method that takes no paraments
   def virus_effects
     predicted_deaths(@population_density, @population, @state)
     speed_of_spread(@population_density, @state)
   end
-
+#define private method that does not expose to the public API
   private
-
+#instace method with 3 parameters, and calculate the number of death base on the population density. 
+#Print outs the result of it.
   def predicted_deaths(population_density, population, state)
     # predicted deaths is solely based on population density
     if @population_density >= 200
@@ -40,7 +42,8 @@ class VirusPredictor
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
-
+#instance method takes 2 parameters, and calculate the speed of spread.
+#prints out the result.
   def speed_of_spread(population_density, state) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
@@ -69,8 +72,9 @@ end
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
-
+#create an instance virus predictor by using the consatant of STATE_DATA 
 alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+#calling instance method of virus effect on instance of a class
 alabama.virus_effects
 
 jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])

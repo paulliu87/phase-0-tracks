628e71efe8f2ddbb47412b97b6cee9d469aa2709
Paul Liu <paul.pliu87@gmail.com>
2016-08-27 10:50:22 -0600

    add two arrays with horse name and color

---
+++ js/data_structures.js
@@ -0,0 +1,15 @@
+var horse_colors = ["white","black","brown","grey"];
+var horse_names = ["Ed","Flash","Biscuit","Pony"];
+
+//function add_color(new_color){
+
+//}
+console.log(horse_colors);
+console.log(horse_names);
+
+
+horse_colors.push("Red");
+horse_names.push("Pearl");
+
+console.log(horse_colors);
+console.log(horse_names);

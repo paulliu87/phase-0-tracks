fdb7d93e339d2be72b42d684f7f253d566051402
Paul Liu <paul.pliu87@gmail.com>
2016-08-27 11:10:40 -0600

    release1 build object

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,12 +4,21 @@ var horse_names = ["Ed","Flash","Biscuit","Pony"];
 //function add_color(new_color){
 
 //}
-console.log(horse_colors);
-console.log(horse_names);
+//console.log(horse_colors);
+//console.log(horse_names);
 
 
 horse_colors.push("Red");
 horse_names.push("Pearl");
 
-console.log(horse_colors);
-console.log(horse_names);
+//console.log(horse_colors);
+//console.log(horse_names);
+//console.log(horse_names.length);
+
+var horse_list = {};
+
+for (var i = 0; i < horse_names.length; i+=1){
+	horse_list[horse_names[i]] = horse_colors[i];
+}
+
+console.log(horse_list);

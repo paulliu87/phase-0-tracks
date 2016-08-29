4da0e31bfc82b83414895986d3eeabe019c08129
Paul Liu <paul.pliu87@gmail.com>
2016-08-28 21:46:12 -0600

    add pseudocode

---
+++ js/algos.js
@@ -0,0 +1,6 @@
+//create the array
+//define a for loop that runs as many times as the length of array
+//define a variable that has max length
+//if the element has longer length than the variable, update it
+//return and stop the loop
+

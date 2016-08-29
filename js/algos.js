164a46d5d735aa7357b26ecc37f76a0d252ae6a5
Paul Liu <paul.pliu87@gmail.com>
2016-08-28 22:41:38 -0600

    Release1

--- js/algos.js
+++ js/algos.js
@@ -1,4 +1,6 @@
-//create the array
+
+//Release 0
+//define a function that takes one parameter
 //define a for loop that runs as many times as the length of array
 //define a variable that has max length
 //if the element has longer length than the variable, update it
@@ -16,4 +18,32 @@ function find_longest_phrase(sample) {
 	return longest_phase;
 }
 
+
+//Release1
+//define a function that takes two parameters
+//define a loop that runs as many times as the length of first input
+//compare each element from first parameter to the second parament to see if there is any same one
+//return a true or false as output
+//driver code
+
+function find_key_value_match(sample_1,sample_2){
+	var compare_match = false;
+	var keys_1 = Object.keys(sample_1);
+	var keys_2 = Object.keys(sample_2);
+	for(var i = 0; i<keys_1.length;i++){
+		for(var j = 0; j <keys_2.length; j++){
+			if(keys_1[i] == keys_2[j]){
+				if(sample_1[keys_1[i]]==sample_2[keys_2[j]]){
+				compare_match =true;
+				break;
+			}}			
+		}
+	}
+	return compare_match;
+}
+
+//driver code for release 0
 console.log(find_longest_phrase(["long phrase","longest phrase","longer phrase","eiorjaiojioejeaiojaier"]));
+//driver code for elease 1
+
+console.log(find_key_value_match({name: "Steven", age: 54},{name: "Tamir", age: 53}));

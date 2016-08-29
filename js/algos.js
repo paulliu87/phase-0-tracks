ce2fa9f0ce9950c387ea85be8895c02f74b31fec
Paul Liu <paul.pliu87@gmail.com>
2016-08-28 21:53:11 -0600

    release1

--- js/algos.js
+++ js/algos.js
@@ -4,3 +4,16 @@
 //if the element has longer length than the variable, update it
 //return and stop the loop
 
+
+function find_longest_phrase(sample) {
+	var longest_phase = "";
+	for (var i = 0; i < sample.length; i++){
+		if (sample[i].length > longest_phase.length){
+			longest_phase = sample[i];
+		}
+	}
+
+	return longest_phase;
+}
+
+console.log(find_longest_phrase(["long phrase","longest phrase","longer phrase","eiorjaiojioejeaiojaier"]));

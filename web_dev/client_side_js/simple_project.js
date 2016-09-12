40fa0e83f59ee736e18a5db194530f5c9f8beec6
Paul Liu <paul.pliu87@gmail.com>
2016-09-11 22:23:29 -0600

    release2

--- web_dev/client_side_js/simple_project.js
+++ web_dev/client_side_js/simple_project.js
@@ -6,6 +6,13 @@ function addPinkBorder(event) {
   event.target.style.border = "2px solid pink";
 }
 
+function invisible(event){
+	photo.style.visibility="hidden";
+}
+function visible(event){
+	photo.style.visibility="visible";
+}
+
 var photo = document.getElementById("lizard-photo");
 photo.addEventListener("click", addPinkBorder);
 
@@ -14,7 +21,12 @@ var color = colors[0];
 //function changecolor(){
 color.style.backgroundColor ="#008000";
 //}
-
 //changecolor();
+
+var button_one = document.getElementById("b1");
+button_one.addEventListener("click",invisible);
+var button_two = document.getElementById("b2");
+button_two.addEventListener("click",visible);
+
 
 
